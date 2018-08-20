e545596df30e74ffb0300e130d2de12266810107
moedes <jmozes50@gmail.com>
2018-08-19 19:07:39 -0700

    Reconfiguration of Vagrantfile to add puppet modules and manifests

--- nginx/nginx.sh
+++
@@ -1,7 +0,0 @@
-apt-get -y update
-
-apt-get -y install nginx
-
-cp /home/vagrant/vagrant /etc/nginx/sites-enabled/default
-
-service nginx start 
---
+++ nginx/puppet-install.sh
@@ -0,0 +1,5 @@
+wget https://apt.puppetlabs.com/puppet5-release-xenial.deb
+dpkg -i puppet5-release-xenial.deb
+apt-get update
+apt-get install -y puppet-agent
+
